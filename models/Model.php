34ead5dc58b119c55dd4e875cb6fc923c4fd2dbd
enerjiser2 <project@idefclub.ru>
2015-04-25 21:44:29 +0300

    сделал метод insert()

--- models/Model.php
+++ models/Model.php
@@ -1,52 +1,59 @@
 <?php
 
-require_once __DIR__ . '/../class/Db.php';
-
 abstract class Model
 {
     protected $magicProp = [];
 
     public $id;
 
+
     public static function getTable()
     {
         return static::$table;
     }
 
-    public static function getAllRecords()
-    {
-        $class = static::class;
-        $sql = 'SELECT * FROM ' . static::getTable();
-        $db = new Db(__DIR__ . '/../siteconfig.php');
-        return $db->getRecords($class, $sql);
-    }
-
-    public static function getOneRecord($id)
-    {
-        $class = static::class;
-        $sql = 'SELECT * FROM ' . static::getTable() . ' WHERE id=:id';
-        $db = new Db(__DIR__ . '/../siteconfig.php');
-        return $db->getRecord($class, $sql, [':id' => $id]);
-    }
-
-    public function insert()
-    {
-        echo 'insert()<br>';
-        var_dump($this->magicProp);
-        $class = static::class;
-        foreach ($this->magicProp as $k => $v):
-            echo $class . ' ' . $k . ' = ' . $v;
-        endforeach;
-    }
-
     public function __get($name)
     {
-        return $magicProp[$name];
+        return $this->magicProp[$name];
     }
 
     public function __set($name, $value)
     {
-        $magicProp[$name] = $value;
+        $this->magicProp[$name] = $value;
+    }
+
+    public static function getAllRecords()
+    {
+        $sql = 'SELECT * FROM ' . static::getTable();
+        $db = new Db(Conf::newsDb());
+        return $db->getRecords(static::class, $sql);
+    }
+
+    public static function getOneRecord($id)
+    {
+        $sql = 'SELECT * FROM ' . static::getTable() . ' WHERE id=:id';
+        $db = new Db(Conf::newsDb());
+        return $db->getRecord(static::class, $sql, [':id' => $id]);
+    }
+
+    public function insert()
+    {
+        $keys = array_keys($this->magicProp);
+        $vals = array_values($this->magicProp);
+
+        $sql = 'INSERT INTO ' . static::getTable() . '
+                (' . '`' . implode($keys, '`,`') . '` )
+                VALUES
+                (' . '\'' . implode($vals, '\',\'') . '\' )
+                ';
+        echo $sql;
+        $db = new Db(Conf::newsDb());
+        return $db->sqlExec($sql);
+    }
+
+    public function update()
+    {
+
     }
 }
 
