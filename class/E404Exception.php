4965b54c6a60eb173d0d27ddd3f0848cf51ee65b
enerjiser2 <project@idefclub.ru>
2015-04-27 20:56:59 +0300

    full commit

--- class/E404Exception.php
+++ class/E404Exception.php
@@ -1,10 +1,12 @@
 <?php
+session_start();
 
 class E404Exception
     extends Exception
 {
     public function redirect($name)
     {
+        $_SESSION['error'] = 'Файл ' .$name . 'не найден';
         header('Location: ./error.php');
     }
 }
