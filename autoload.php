6cde1abe4f5e989010529535a617327ce22bf53f
enerjiser2 <project@idefclub.ru>
2015-04-30 20:16:37 +0300

    full commit

--- autoload.php
+++ autoload.php
@@ -4,9 +4,28 @@ function __autoload($class)
 {
     if (false !== strpos($class, '\\')) {
         $classNameParts = explode('\\', $class);
+    //    var_dump($classNameParts);
         if ('App' == $classNameParts[0]) {
             unset($classNameParts[0]);
+
+            $paths = [
+                __DIR__ . '/class',
+                __DIR__ . '/controllers',
+                __DIR__ . '/models',
+            ];
+
+            foreach($paths as $path):
+                unset($classNameParts[1]);
+                $fileName = $path. '/' . implode('/', $classNameParts) . '.php';
+
+                if (file_exists($fileName)) {
+                    echo $fileName . '<br>';
+                    require_once $fileName;
+                }
+            endforeach;
+
             $fileName = __DIR__ . '/' . implode('/', $classNameParts) . '.php';
+
             if (file_exists($fileName)) {
                 require_once $fileName;
                 return true;
